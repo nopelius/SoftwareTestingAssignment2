c2eac9f8ab1678e95ca994066e54b9cacaf46960
Tuomas Ahvenus <tuomas.ahvenus@tuni.fi>
2020-12-05 21:09:41 +0200

    Added numerous tests. They may need to be moved later

---
+++ src/test/slice.test.js
@@ -0,0 +1,5 @@
+import slice from'../slice.js';
+
+test('array [1, 2, 3, 4] is sliced from 2 to end', () => {
+    expect(slice([1, 2, 3, 4], 2)).toBe([3, 4]);
+});
---
+++ src/test/words.test.js
@@ -0,0 +1,9 @@
+import words from'../words.js';
+
+test('fred, barney, & pebbles is returned as an array of strings fred, barney, pebbles', () => {
+    expect(words('fred, barney, & pebbles').toBe(['fred', 'barney', 'pebbles']));
+});
+
+test('string fred, barney, & pebbles and /[^, ]+/g are returned as an array of strings fred, barney, &, pebbles', () => {
+    expect(words('fred, barney, & pebbles', /[^, ]+/g).toBe(['fred', 'barney', '&', 'pebbles']));
+});
