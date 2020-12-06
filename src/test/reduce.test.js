e4925e34e12f58d1008ab2c030e87a5a57c400e9
Nooa Jekunen <nopelius.jekunen@gmail.com>
2020-12-06 15:20:49 +0200

    Deleted duplicates

--- src/test/reduce.test.js
+++
@@ -1,5 +0,0 @@
-import reduce from'../reduce.js';
-
-test('array [1, 2] is reduced with iteratee (sum, n) => sum + n, starting from 0', () => {
-	expect(reduce([1, 2], (sum, n) => sum + n, 0).toBe(3));
-});
