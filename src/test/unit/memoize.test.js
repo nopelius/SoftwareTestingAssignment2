4490f054dc02d59b6740c5eedfd3851aa1b9f64b
Nooa Jekunen <nopelius.jekunen@gmail.com>
2020-12-07 18:37:53 +0200

    Memoize test added

--- src/test/unit/memoize.test.js
+++ src/test/unit/memoize.test.js
@@ -4,26 +4,28 @@ import memoize from'../../memoize.js';
 const object = { 'a': 1, 'b': 2 };
 const other = { 'c': 3, 'd': 4 };
 
-const values = () => { 
-	memoize(values);
-	values.cache = Map;
-};
+function values(anObject){
+	let arr = [];
+	for(let prop in anObject){
+		arr.push(anObject[prop]);
+	}
+	return arr;
+}
+
+const memoizedValues = memoize(values);
 
 test('{ a: 1, b: 2 } is memoized and its values become [1, 2]', () => {
-	expect(values(object)).toBe([1, 2]);
+	expect(memoizedValues(object)).toEqual([1, 2]);
 });
 
 test('{ c: 3, d: 4 } is memoized and its values become [3, 4]', () => {
-	expect(values(other)).toBe([3, 4]);
+	expect(memoizedValues(other)).toEqual([3, 4]);
 });
 
-// Test values are altered
-//values.cache = new Map();
-values.cache.set(object, ['a', 'b']);
+//Test values are altered
 
 test('{ a: a, b: b } is memoized and its values become [a, b]', () => {
-	expect(values(object)).toBe(['a', 'b']);
+	console.log(memoizedValues.cache);
+	memoizedValues.cache.set(object, ['a', 'b']);
+	expect(memoizedValues(object)).toEqual(['a', 'b']);
 });
-
-// memoize.Cache is replaced
-memoize.Cache = WeakMap;
